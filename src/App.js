002ffafc7e03446a60e04286ef91c23646e69e65
neophytte <neophytte@westnet.com.au>
2020-12-09 11:45:00 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import dummyData from "./data";
 
-function App() {
+import CardList from "./components/CardList";
+import SkeletonCard from "./components/SkeletonCard";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Load this effect on mount
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setVideos(dummyData);
+      setLoading(false);
+    }, 10000);
+    // Cancel the timer while unmounting
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <SkeletonCard />}
+      {!loading &&
+        videos.map((list, index) => {
+          return (
+            <section key={index}>
+              <h2 className="section-title">{list.section}</h2>
+              <CardList list={list} />
+              <hr />
+            </section>
+          );
+        })}
     </div>
   );
-}
+};
 
 export default App;
